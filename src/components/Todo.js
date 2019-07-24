7b875fb20c6f47d9552eaa22ec3eac7ff85ac928
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-07-24 13:13:31 +0100

    Implement array destructuring with useState()

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,41 @@
 import React, { useState } from "react";
 
 const todo = props => {
-  const inputState = useState("");
+  //const [inputValue, updateValue] = useState("");
+  //const [todoList, updateTodoList] = useState([]);
+
+  const [todoState, setTodoState] = useState({ inputValue: "", todoList: [] });
 
   const inputChangeHandler = event => {
-    inputState[1](event.target.value);
+    setTodoState({
+      inputValue: event.target.value,
+      todoList: todoState.todoList
+    });
   };
-  console.log(inputState);
+
+  const todoAddHandler = () => {
+    setTodoState({
+      inputValue: todoState.inputValue,
+      todoList: todoState.todoList.concat(todoState.inputValue)
+    });
+  };
+
   return (
     <React.Fragment>
       <input
         type="text"
         placeholder="Todo"
         onChange={inputChangeHandler}
-        value={inputState[0]}
+        value={todoState.inputValue}
       />
-      <button type="button">Add</button>
-      <ul />
+      <button type="button" onClick={todoAddHandler}>
+        Add
+      </button>
+      <ul>
+        {todoState.todoList.map(todo => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
     </React.Fragment>
   );
 };
