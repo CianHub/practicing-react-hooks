1b47a9c731143b8c9249c0430c12c68bbb69f57d
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-07-24 12:40:01 +0100

    add useState

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const todo = props => {
-  return <div>Hello</div>;
+  const inputState = useState("");
+
+  const inputChangeHandler = event => {
+    inputState[1](event.target.value);
+  };
+  console.log(inputState);
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        placeholder="Todo"
+        onChange={inputChangeHandler}
+        value={inputState[0]}
+      />
+      <button type="button">Add</button>
+      <ul />
+    </React.Fragment>
+  );
 };
 
 export default todo;
